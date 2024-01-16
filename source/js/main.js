42fc7f55f39b4d5e35d923ef2b180971d7a6562b
mgDentist <gmi1987@gmail.com>
2024-01-16 15:58:01 +0400

    Вносит стили и аккордеон в header блок

--- source/js/main.js
+++ source/js/main.js
@@ -4,5 +4,28 @@
 // import 'swiper/css';
 
 import { navOpener } from "./modules/burger";
+import { initAccordions } from "./vendor/accordion/init-accordion";
 
-navOpener();
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  // iosVhFix();
+
+  // Modules
+  // ---------------------------------
+  navOpener();
+
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    // initModals();
+    // const form = new Form();
+    // window.form = form;
+    // form.init();
+    // initTabs();
+    initAccordions();
+  });
+});
