e6720401c0abb14d8fc18c3a3749a07466e579fd
mgDentist <gmi1987@gmail.com>
2024-01-22 14:18:40 +0400

    Заканчивает блок programs

--- source/js/modules/slider-programs.js
+++ source/js/modules/slider-programs.js
@@ -26,7 +26,7 @@ const initProgramsSlider = () => {
       // },
 
       autoHeight: true,
-      loop: true,
+      // loop: true,
       breakpoints: {
         1440: {
           allowTouchMove: true,
